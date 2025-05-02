9466059d0745cb525af2261fecd699f07dc45db9
elkythn-web <elkinportillo5@gmail.com>
2025-05-02 09:31:43 -0600

    Arreglando la ruta de Categorias

--- routes/products/categoryRoute.ts
+++ routes/products/categoryRoute.ts
@@ -15,4 +15,5 @@ router.put('/update/:category_id', updateCategory);
 router.put('/update-status/:category_id', updateCategoryStatus);
 router.delete('/delete/:category_id', deleteCategory);
 
+
 export default router;
